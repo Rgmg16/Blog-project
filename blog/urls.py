3c0b656b022bbdde97b9c0a2fe9b2ae98e807211
Rgmg16 <gumborobert7@gmail.com>
2024-07-04 20:44:09 +0300

    Updates to the project

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('blog/', views.blog_list, name='blog_list'),
     path("editors/",views.editor_list, name="editor_list"),
+    path('add_blog', views.add_blog, name='add_blog')
 ]
